3b4e39cc08e846be1e44d9ca73339798e3a3c0dc
Supslike <80964103+Supslike@users.noreply.github.com>
2024-01-16 18:40:09 +0800

    Adjusted file names

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,42 +1,5 @@
-let creepy_guy = document.getElementById("cripay");
-// Get all elements on the page
-var allElements = document.querySelectorAll('*');
-
-
-function hasOneInTwoHundredChance() {
-    // Generate a random number between 1 and 200
-    const randomNumber = Math.floor(Math.random() * 200) + 1;
-  
-    // Check if the random number equals 1
-    return randomNumber === 1;
-  }
-
 // Function to reveal elements based on scroll position
 function reveal() {
-    let value = window.scrollY;
-    // let side = window.scrollX;
-    // if (side > 0) {
-    //     window.scroll(0, window.scrollY);
-    //     document.documentElement.style.overflowX = "hidden"
-    //     document.documentElement.style.scrollBehavior = "auto"
-    // };
-    // if (side == 0) {
-    //     document.documentElement.style.overflowX = "visible"
-    //     document.documentElement.style.scrollBehavior = "smooth"
-    // };
-
-
-    // if (value * 0.1 <= 30) {
-    //     creepy_guy.style.transform = `translateY(${value}px) rotate(-${value * 0.1}deg)`;
-    // }
-    // else {
-    //     creepy_guy.style.transform = `translateY(${value}px) rotate(-30deg)`;
-    // };
-
-    // if (hasOneInTwoHundredChance()) {
-    //     creepy_guy.classList.add("eyes")
-    // };
-
     // Selecting elements with class "reveal", "warning", and "animate"
     var reveals = document.querySelectorAll(".reveal");
 
